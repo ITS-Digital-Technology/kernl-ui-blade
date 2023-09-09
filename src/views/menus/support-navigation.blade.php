a3374334fa5fad841c262da0e3c96cf50c961fa3
Cameron Van Orman <cameron.vanorman@gmail.com>
2023-09-09 00:26:51 -0700

    Update includes for support-naviagtion

--- src/views/menus/support-navigation.blade.php
+++ src/views/menus/support-navigation.blade.php
@@ -2,9 +2,9 @@
     @foreach ($supportNav as $item)
         @isset ($item['children'])
             @if(count($item['children']) > 0)
-                @include('kernl-ui::includes.menus.mega-menu.support-navigation.link-with-children', ['item' => $item])
+                @include('kernl-ui::includes.menus.support-navigation.link-with-children', ['item' => $item])
             @else
-                @include('kernl-ui::includes.menus.mega-menu.support-navigation.link', ['item' => $item])
+                @include('kernl-ui::includes.menus.support-navigation.link', ['item' => $item])
             @endif
         @endif
     @endforeach
