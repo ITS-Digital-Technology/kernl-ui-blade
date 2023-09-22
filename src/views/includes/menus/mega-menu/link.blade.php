7fc5711b3307c265d867ba6fa95fe5c8ca83278b
Cameron Van Orman <cameron.vanorman@gmail.com>
2023-09-22 15:17:15 -0700

    Update mega-menu link with px-4 padding

--- src/views/includes/menus/mega-menu/link.blade.php
+++ src/views/includes/menus/mega-menu/link.blade.php
@@ -3,7 +3,7 @@
         href="{{ $item['href'] }}"
     @endif
     class="
-        inline-flex items-center pl-4 py-1 text-sm rounded-sm 
+        inline-flex items-center px-4 py-1 text-sm rounded-sm 
         hover:bg-gray-100 transition duration-200 hover:text-gray-800 hover:cursor-pointer focus:outline-none focus:ring focus:ring-blue-400
         {{ $dark ? 'text-white' : 'text-gray-800' }}
     "
