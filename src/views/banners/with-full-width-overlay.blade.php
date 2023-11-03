42fd4b8d7d75e3fdb0662b18ecb97e351d2b9d88
Cameron Van Orman <cameron.vanorman@gmail.com>
2023-11-02 17:10:08 -0700

    Update class attribute in with-full-width-overlay banner component template

--- src/views/banners/with-full-width-overlay.blade.php
+++ src/views/banners/with-full-width-overlay.blade.php
@@ -1,12 +1,9 @@
 <div
-    class="
-        {{ $compiledClasses }} 
-        bg-center bg-cover bg-no-repeat
-    "
+    {{ $compiledClasses }}
 
     {!! 
         $backgroundUrl || !isset($backgroundUrl)
-            ? ' style="background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(' . $backgroundUrl . ')"' 
+            ? 'style="background-image: linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url(' . $backgroundUrl . ')"' 
             : ''
     !!}
 >
